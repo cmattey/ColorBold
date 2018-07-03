ecb4cfe88357cc428b450e9ed41a14e9a41c6616
cmattey <cmattey@dons.usfca.edu>
2018-07-03 13:44:01 -0700

    Added example images and cleaned code

--- tkinter_gui.py
+++ tkinter_gui.py
@@ -3,22 +3,20 @@ from PIL import Image
 from PIL import ImageTk
 import cv2
 import numpy as np
-# import tkFileDialog
 from tkinter import filedialog
 window = Tk()
 window.title("ColorBold")
-# lbl = Label(window, text="Hello")
-#
-# lbl.grid(column=0, row=0)
+
 # Gamma correction is also known as the Power Law Transform.
 # First, our image pixel intensities must be scaled from the range [0, 255] to [0, 1.0].
 # From there, we obtain our output gamma corrected image by applying the following equation:
 # O = I ^ (1 / G)
 # Where I is our input image and G is our gamma value. The output image O is then scaled back to the range [0, 255].
 
+
 def adjust_gamma(image, gamma=1.0):
-    invGamma = 1.0 / gamma
-    table = np.array([((i / 255.0) ** invGamma) * 255
+    inv_gamma = 1.0 / gamma
+    table = np.array([((i / 255.0) ** inv_gamma) * 255
                       for i in np.arange(0, 256)]).astype("uint8")
     # https: // www.pyimagesearch.com / 2015 / 10 / 05 / opencv - gamma - correction /
     # Gamma values < 1 will shift the image towards the darker end of the spectrum
@@ -28,6 +26,7 @@ def adjust_gamma(image, gamma=1.0):
 
     # apply gamma correction using the lookup table
     return cv2.LUT(image, table)
+
 
 def clicked():
     global lbl
@@ -49,6 +48,7 @@ def clicked():
     lbl = Label(window, image=imgtk, width = 500, height = 500)
     lbl.image = imgtk
     lbl.grid(column=0, row=0)
+
 
 def show_red():
     global lbl
@@ -76,6 +76,7 @@ def show_red():
     lbl.image = imgtk
     lbl.grid(column=0, row=0)
 
+
 def show_green():
     global lbl
     img = cv2.imread(path)
@@ -85,37 +86,32 @@ def show_green():
 
     for i in range(img.shape[0]):
         for j in range(img.shape[1]):
-            if (img[i, j, 2] < 100 and img[i, j, 1] >80 and img[i, j, 0] < 100):
+            if img[i, j, 2] < 100 and img[i, j, 1] >80 and img[i, j, 0] < 100:
                 light_img[i, j] = img[i, j]
 
-     # Rearrang the color channel
+    # Rearrange the color channel
     b, g, r = cv2.split(light_img)
     light_img = cv2.merge((r, g, b))
 
     im = Image.fromarray(light_img)
     imgtk = ImageTk.PhotoImage(image=im)
 
-    # im_temp = imgtk.resize((250, 250), Image.ANTIALIAS)
-
     # Put it in the display window
     lbl = Label(window, image=imgtk, width=500, height=500)
     lbl.image = imgtk
     lbl.grid(column=0, row=0)
 
+
 def show_original():
     global lbl
     img = cv2.imread(path)
-    # print(path)
-    # print("yolo")
 
-    # Rearrang the color channel
+    # Rearrange the color channel
     b, g, r = cv2.split(img)
     img = cv2.merge((r, g, b))
 
     im = Image.fromarray(img)
     imgtk = ImageTk.PhotoImage(image=im)
-
-    # im_temp = imgtk.resize((250, 250), Image.ANTIALIAS)
 
     # Put it in the display window
     lbl = Label(window, image=imgtk, width=500, height=500)
@@ -136,10 +132,6 @@ btn_green.grid(column=1, row=2)
 
 btn_show = Button(window, text="Show Original",bg="orange", fg="black",command=show_original)
 btn_show.grid(column=1, row=3)
-
-# btn = Button(window, text="Select an image", command=select_image)
-# btn.grid(column=0, row=1)
-# btn.pack(side="bottom", fill="both", expand="yes", padx="10", pady="10")
 
 window.geometry('1000x1000')
 window.mainloop()
